63739e05d097a914fb2f13f26af862f9319c2d62
NataliiaDanchenko <127970410+NataliiaDanchenko@users.noreply.github.com>
2025-12-06 05:12:47 +0200

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    // @ts-expect-error appDir пока не типизировано
+    appDir: true, // если используешь app/
+  },
+  images: {
+    // если нужны локальные SVG/PNG из assets
+    unoptimized: true, // можно убрать, если не используешь оптимизацию
+  },
 };
 
 export default nextConfig;
